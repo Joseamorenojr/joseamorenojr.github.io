1ef7b320c48bb996b653d91e4842a5af64cb367d
Jose Antonio Moreno <100990781+Joseamorenojr@users.noreply.github.com>
2026-03-08 03:40:26 -0500

    Update page-transition.js

--- assets/js/page-transition.js
+++ assets/js/page-transition.js
@@ -21,22 +21,18 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function toggleTheme(){
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
 
-document.body.classList.toggle("dark-mode");
-
-if(document.body.classList.contains("dark-mode")){
-localStorage.setItem("theme","dark");
-}else{
-localStorage.setItem("theme","light");
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme","dark");
+  } else {
+    localStorage.setItem("theme","light");
+  }
 }
 
-}
-
-window.onload = function(){
-
-if(localStorage.getItem("theme")==="dark"){
-document.body.classList.add("dark-mode");
-}
-
+window.onload = function() {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+  }
 }
